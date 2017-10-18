9374f9b8177c684e9ef05e49c5b634ae64c9777c
Amy Russell <russell.amyh@gmail.com>
2017-10-18 17:12:12 -0600

    Crushed

--- script.js
+++ script.js
@@ -51,12 +51,72 @@ var enterButton = document.querySelector('.enter');
 var bookmarkList = document.querySelector('.bookmark-list');
 var title = document.querySelector('.website-title');
 var url = document.querySelector('.website-url');
+var bookmarkCounter = document.querySelector('.bookmark-counter');
+var readCounter = document.querySelector('.read-counter');
+var unreadCounter = document.querySelector('.unread-counter');
 
+// need to make an event.preventDefault 
 
 // event listeners
 enterButton.addEventListener('click', userInput); 
+bookmarkList.addEventListener('click', function(event){
+if (event.target.className ==='unread' || event.target.className ==='unread read'){
+  event.target.classList.toggle('read');
+  event.target.closest('article').classList.toggle('read-card');
+  countBookmarks();
+} 
+  if (event.target.className === 'delete'){
+    event.target.closest('article').remove();
+    countBookmarks();
+  }
+})
 
 
+// functions
+function countBookmarks(){
+  var bookmarkCount = document.querySelectorAll('article').length -1;
+  var readCardCount = document.querySelectorAll('.read-card').length;
+  var unreadCardCount = bookmarkCount - readCardCount;
+  bookmarkCounter.innerText = bookmarkCount;
+  readCounter.innerText = readCardCount;
+  unreadCounter.innerText = unreadCardCount;
+}
+
+
+title.addEventListener('keyup', function()
+{
+  console.log("you're typing")
+  if (title.value === "" || url.value === "") {
+    enterButton.setAttribute('disabled',true);
+    console.log("I'm disabled");
+}
+  else  {
+    enterButton.removeAttribute('disabled');
+    console.log("I'm not disabled anymore");
+}
+});
+
+url.addEventListener('keyup', function(){
+  console.log("you're still tryping")
+if (title.value === "" || url.value === "") {
+  enterButton.setAttribute('disabled',true)
+}
+else {
+  enterButton.removeAttribute('disabled')
+}
+  
+});
+
+
+// function validateURL () {
+//   var urlValue = url.value;
+//   var regEx = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/;
+
+//   urlValue.test(regex):
+// }
+
+// if values are both there, then an enable button
+// regex-copy and paste it.
 
 function userInput(event) {
   event.preventDefault();
@@ -68,6 +128,7 @@ function userInput(event) {
     console.log (card)
   console.log('negative nancy')
   bookmarkList.innerHTML+=(makeCard(card));
+  countBookmarks();
 }
 // += adds on to what you already have in there!
 function Bookmark(title, url) { 
@@ -85,20 +146,26 @@ function Bookmark(title, url) {
   function makeCard(completedCard) {
     var newTitle = completedCard.title;
     var newUrl = completedCard.url;
+    // var actualURL;
+    // if(/^(http[s]?:\/\/)/.test(link) === false) {
+    //     link = 'https://' + link;
+    // }
+
     console.log (newUrl)
-    return (`<article class="bookmark-card">
+    return (`<article class='bookmark-card'>
       <h2 class="website-title">${newTitle}</h2>
       <hr>
-      <a class="url" href="${newUrl}">${newUrl}</a>
+      <a class='url' href="${newUrl}">${newUrl}</a>
       <hr>
-      <button class="read">Read</button>
-      <button class="delete">Delete</button>
+      <button class='unread'>Read</button>
+      <button class='delete'>Delete</button>
     </article>`)
   }
  
- document.querySelector(".bookmark-list").addEventListener('click',function() {
+ document.querySelector('.bookmark-list').addEventListener('click',function() {
  this.target
 });
+
 // give id to the article. h5 onClick= ''
  // checkValidity function- 
 
